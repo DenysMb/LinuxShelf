32dccb1912d98a8b0ddfc4a0bffa3461cd042ade
Denys M. Barbosa <dbarbosa@i-hunter.com>
2019-01-03 08:53:22 -0200

    Componente estilizado de Upload do iView

--- src/components/AddAppCard.vue
+++ src/components/AddAppCard.vue
@@ -1,25 +1,26 @@
 <template>
-  <Card>
-    <div style="text-align:center">
-      <img
-        src="@/assets/arrow.png"
-        class="dropHere"
-      />
-      <h3 style="opacity: 0.2;">Drop here</h3>
-    </div>
-  </Card>
+  <Upload
+        ref="upload"
+        :format="['deb']"
+        multiple
+        type="drag"
+        action="//jsonplaceholder.typicode.com/posts/"
+        style="display: inline-block;width:160px;height:190px;">
+        <Card>
+          <div style="text-align:center">
+            <Icon type="ios-cloud-upload" class="dropHere" size="108"></Icon>
+            <h3 style="opacity: 0.2;">Click or Drop here</h3>
+          </div>
+        </Card>
+    </Upload>
 </template>
 
 <script>
-  export default {}
+export default {};
 </script>
 
 <style scoped>
-  .dropHere {
-    width: 128px;
-    height: 128px;
-    border: 5px dotted black;
-    border-radius: 20px;
-    opacity: 0.1;
-  }
+.dropHere {
+  opacity: 0.1;
+}
 </style>
